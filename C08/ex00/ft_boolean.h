31a737bba30a71ff8b9bad734524231a989fb676
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 17:33:02 +0200

    Create ft_boolean.h

---
+++ C08/ex00/ft_boolean.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhers <dhers@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/19 17:14:45 by dhers             #+#    #+#             */
+/*   Updated: 2020/08/20 20:04:24 by dhers            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+# define FALSE 0
+# define TRUE 1
+# define EVEN(x) (x % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int		t_bool;
+#endif
