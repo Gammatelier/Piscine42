23a9311e7a66b2ba8be2a3daa6404bd1213d729c
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 17:32:42 +0200

    Create ft.h

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhers <dhers@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/19 17:03:52 by dhers             #+#    #+#             */
+/*   Updated: 2020/08/19 17:45:50 by dhers            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
