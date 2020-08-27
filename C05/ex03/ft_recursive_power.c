c18ac4bfa702a730198e0985737930ab6427775b
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 16:46:22 +0200

    Create ft_recursive_power.c

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhers <dhers@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/13 15:49:18 by dhers             #+#    #+#             */
+/*   Updated: 2020/08/16 18:12:29 by dhers            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else
+		return (nb *= ft_recursive_power(nb, power - 1));
+}
