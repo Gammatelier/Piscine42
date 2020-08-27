4465d42e61a477378451543903db7e933b533954
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 14:59:45 +0200

    Create r_dwssap.sh

---
+++ Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | sed "/^#/d" | sed -n "n;p" | cut -d: -f1-1 | rev | sort -r | awk -v var1=$FT_LINE1 -v var2=$FT_LINE2 'NR >= var1 && NR <= var2' | sed 's/$/, /' | tr -d '\n' | sed 's/, $/./' | tr -d '\n'
