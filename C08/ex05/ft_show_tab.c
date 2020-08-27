6ecb4408600feb6a7d0d679fc39e3b4a09bcb975
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 17:37:50 +0200

    Create ft_show_tab.c

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhers <dhers@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/20 16:31:32 by dhers             #+#    #+#             */
+/*   Updated: 2020/08/21 14:30:26 by dhers            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	nb = nb + '0';
+	ft_putchar(nb);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int i;
+
+	i = 0;
+	while (par[i].str != '\0')
+	{
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+
+///////////////////////////////////////////////
+ft_stock_str.h
+
+typedef struct s_stock_str
+{
+int size;
+char *str;
+char *copy;
+}             t_stock_str;
