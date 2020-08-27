d6d00c700533730e5d1d8bc4129e4ab799069fe8
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 16:47:11 +0200

    Create ft_fibonacci.c

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhers <dhers@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/13 16:11:44 by dhers             #+#    #+#             */
+/*   Updated: 2020/08/13 16:43:44 by dhers            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
