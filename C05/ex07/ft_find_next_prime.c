05f236a6909fa96253e1b14c33515b1222a423d4
Gammatelier <68304282+Gammatelier@users.noreply.github.com>
2020-08-27 23:49:59 +0200

    Update ft_find_next_prime.c

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -35,7 +35,7 @@ int		ft_is_prime(int nb)
 
 int		ft_find_next_prime(int nb)
 {
-	if (nb < 2).  //because first prime for negative number is 2
+	if (nb < 2)  //because first prime for negative number is 2
 		return (2);
 	while (ft_is_prime(nb) != 1)
 		nb++;
